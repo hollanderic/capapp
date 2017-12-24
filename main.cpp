f53b2606a03ca28424ac2596cb1175b66ff351d4
Eric Holland <eric.holland@gmail.com>
2017-12-24 18:51:54 -0500

    change test app to use rgb methods

--- main.cpp
+++ main.cpp
@@ -17,20 +17,20 @@ int main(int argc, char** argv ){
     printf("Could not open file %s!\n",argv[1]);
     return -1;
   }
-  printf("opened fit\n");
 
   //cv::Mat 8mat(height, width, CV_16UC1, inputBuffer);
-  int height = fit->getHeight();
-  int width = fit->getWidth();
+  //int height = fit->getHeight();
+  //int width = fit->getWidth();
 
-  cv::Mat im_rgb_ = cv::Mat(height, width, CV_16UC3);
-  cv::cvtColor(fit->getMat(), im_rgb_, cv::COLOR_BayerRG2RGB);
+  //cv::Mat im_rgb_ = cv::Mat(height, width, CV_16UC3);
+  //cv::cvtColor(fit->getMat(), im_rgb_, cv::COLOR_BayerRG2RGB);
 
   //im_rgb16_.convertTo(im_rgb_, CV_8UC3, 1.0/256);
 
-  cv::Mat im_preview_ = cv::Mat(height/4, width/4, CV_8UC3);
-  resize(im_rgb_, im_preview_, im_preview_.size());
-  imshow( window, im_preview_);
+  //cv::Mat im_preview_ = cv::Mat(height/4, width/4, CV_8UC3);
+  //resize(im_rgb_, im_preview_, im_preview_.size());
+
+  imshow( window, fit->getMatRGB(.4));
   fit->printRecords();
 
   waitKey( 0 );
